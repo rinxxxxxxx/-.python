c93964b3aab7ec6194d5730559c296d3c42d945f
rinxxxxxxx <rin.418049@gmail.com>
2021-03-30 15:14:07 +0800

    my new site

--- datacenter/mysite/views.py
+++ datacenter/mysite/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	myname="森"
+	return render(request,"index.html",locals())
 
 # Create your views here.
